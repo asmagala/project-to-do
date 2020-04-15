b431427fdfac91c9d51a681b0bbeac713aaf6e77
Arkadiusz Smagała <asmagala@outlook.com>
2020-04-16 00:24:51 +0200

    Create Container component and insert it in List and Search.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator.js';
+import Container from '../Container/Container';
 
 class List extends React.Component {
  
@@ -17,6 +18,7 @@ class List extends React.Component {
     columns: PropTypes.array,
     image: PropTypes.node,
     addColumn: PropTypes.func,
+    children: PropTypes.node,
   }
 
   static defaultProps = {
@@ -27,21 +29,22 @@ class List extends React.Component {
     const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={title} pictureSrcText={image} pictureAltText={image}/>
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
+        <Container className={this.props.children}>
+          <Hero titleText={title} pictureSrcText={image} pictureAltText={image}/>
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
           
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn} />
-        </div>
-
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+            
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn} />
+          </div>
+        </Container>  
       </section>
     );
   }
