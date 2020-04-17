27f3a7cfe32bb5fdb046bed46b0fcee120979f2e
Arkadiusz Smagała <asmagala@outlook.com>
2020-04-17 23:32:15 +0200

    Change formatting in Faq.js

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -3,9 +3,14 @@ import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import {settings} from '../../data/dataStore.js';
 
+const { title, image, imageAlt } = settings.faq;
 const Faq = () => (
   <Container>
-    <Hero titleText={settings.faq.title} pictureSrcText={settings.faq.image} pictureAltText={settings.faq.imageAlt}/>
+    <Hero 
+      titleText={title} 
+      pictureSrcText={image} 
+      pictureAltText={imageAlt}
+    />
     <h2>{settings.faq.headerText}</h2>
     <p>{settings.faq.tekst}</p>
   </Container>
